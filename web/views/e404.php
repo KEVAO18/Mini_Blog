eec7d090f22e76e6f067f353a10c5720d9981453
Kevin Andrés Orrego Martínez <51931087+KEVAO18@users.noreply.github.com>
2023-10-30 15:03:34 -0500

    Add files via upload

--- web/views/e404.php
+++ web/views/e404.php
@@ -5,7 +5,7 @@ function e404(){
       <div class="container">
         <div class="row py-4 text-center">
           <div class="col-12 display-1 pt-3">404</div>
-          <div class="col-12 display-4 pb-5">Página no encontrada</div>
+          <div class="col-12 display-4 pb-5">Página no encontrada 1</div>
         </div>
       </div>
 	<?php
