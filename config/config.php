a899371c6538445a2cf0dea6d77644548cb6dbf0
Kevin Andrés Orrego Martínez <51931087+KEVAO18@users.noreply.github.com>
2023-10-30 15:05:57 -0500

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -4,37 +4,12 @@ $autor = "Santiago Orrego Martínez";
 
 $co_autor = "Kevin Andrés Orrego Martínez";
 
-$appName = "Lifestyles, traditions, concepts of beauty";
-
-$email = "example@gmail.com";
+$appName = "LTCB";
 
 $date = date("Y-m-d");
 
-$mes = date('n');
-
-if ($mes >= 2) {
-    $n = date('Y');
-    $ex = $n - 2018;
-    $ag = $n - 2001;
-}else{
-    $nacimiento=2001;
-    $n = date('Y');
-    $ex = $n - 2018;
-    $ag = $n - 2001 - 1;
-}
-
 $icoName = "favicon";
 
-//$host = "localhost";
-
-//$user = "root";
-
-//$pass = "";
-
-//$db = "test";
-
-$serve = "http://localhost/terminadas/santiago/";
-
-//$con = new mysqli($host, $user, $pass, $db) or die("Fallo en la conexion a la base de datos");
+$serve = "https://travelingkev-647109ad13b7.herokuapp.com/";
 
 ?>
